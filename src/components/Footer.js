3bea92f08ae6b85f424560aa6ce0fbf4e0df85da
Alexandra Haynes <sandamunteanua@yahoo.com>
2023-06-07 11:31:09 -1000

    add Terms of Use page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,24 +2,23 @@ import React from "react";
 import { AiOutlineMail } from "react-icons/ai";
 import { BsGithub } from "react-icons/bs";
 import { FaMeetup } from "react-icons/fa";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="w-full bg-gradient-to-b from-yellow-300 to-orange-400">
-      <h4 className="text-center xl:text-4xl text-3xl  pt-10 font-thin">Contact us</h4>
+      <h4 className="text-center xl:text-4xl text-2xl  pt-10 font-thin">
+        Contact us
+      </h4>
 
-      <div className="flex flex-row items-center justify-center py-4">
+      <div className="flex flex-row items-center justify-center py-2">
         <AiOutlineMail fontSize="2rem" />
-
-        <h2 className="pl-2 text-md xl:text-xl">
-          Email:
           <span className="text-md xl:text-xl  px-2">
             info@codeforhawaii.org
           </span>{" "}
-        </h2>
       </div>
 
-      <div className="flex items-center justify-center gap-10  px-20 py-4 text-xl text-center">
+      <div className="flex items-center justify-center gap-10  px-20 py-2 text-xl text-center">
         <a
           href="https://github.com/CodeWithAloha"
           target="_blank"
@@ -35,11 +34,15 @@ const Footer = () => {
           <FaMeetup fontSize={46} />
         </a>
       </div>
-      <div className="flex items-center gap-10  justify-center  py-6 text-md xl:text-xl text-center">
+      <div className="flex flex-col items-center gap-2 
+      justify-center text-sm xl:text-xl pt-6  ">
         <p>Privacy Policy</p>
-        <p>Terms of Use</p>
+
+        <Link href="/terms-of-use">
+         Terms of Use
+        </Link>
       </div>
-      <p className="text-md xl:text-xl font-semibold text-center py-6">
+      <p className="text-sm xl:text-xl font-semibold text-center py-4">
         Copyright © Code With Aloha 2023
       </p>
     </footer>
