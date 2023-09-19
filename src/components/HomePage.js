62e126b379c321911088853550e18ea89e187af4
Kobe <kobetbuckley@gmail.com>
2023-09-18 21:21:08 -1000

    HomePage: Implemented Image from next/image with relevant alt tag for accessibilty

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 import { FaMeetup } from "react-icons/fa";
 import NavBar from './NavBar';
 
@@ -8,7 +9,7 @@ const HomePage = () => {
       <NavBar />
       <div className="max-h-full flex items-center justify-center flex-col lg:flex-row">
         <div className="scale-[80%]  2xl:scale-90 xl:pl-20 flex flex-col items-center">
-          <img src="hero-image.png" className="w-full max-w-[900px] animate-float" />
+          <Image alt='This is a picture of an island with palm trees, a pineapple, coconut, surfboard, flower, starfish, shells, river, sandals, and a laptop.' src="hero-image.png" className="w-full max-w-[900px] animate-float" />
           <div className='w-1/3 h-4 mt-12 rounded-full blur-2xl bg-gradient-to-t bg-black'></div>
         </div>
 
