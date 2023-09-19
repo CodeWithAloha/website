35305b07a225f0fc2c26c2c6ee3c67b2e7dbc3bf
Kobe <kobetbuckley@gmail.com>
2023-09-18 21:27:07 -1000

    Logo: Implemented Image from next/image with relevant alt tag for accessibilty

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,6 +1,6 @@
-import Link from 'next/link'
 import React from 'react'
-
+import Link from 'next/link'
+import Image from 'next/image'
 
 
 
@@ -8,10 +8,11 @@ const Logo = () => {
   return (
     
       <Link href="/" whileHover={{ scale: 1.5 }}>
-        <img
+        <Image
+          alt='This is a logo of Code with Aloha represented by the name, as well as an orange lightbulb with the torch like yellow flame akin to a sunset scene.'
           src="../logo.png"
           className="xl:max-h-24 max-h-[3rem] cursor-pointer m-2 hover:animate-pulse"
-        ></img>
+        />
       </Link>
     
   );
