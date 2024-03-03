d238a5926033e9a5415f2ebb3901b867e1404be0
John Kahookele <kahookelekelii@gmail.com>
2024-03-03 01:12:47 -1000

    This will only work with your own created data analytic TRACKING ID. To test, type 'npm install react-ga4' into terminal and create a tracking ID in google analytics. From there, change tracking ID in index.js file. Please let me know if this is still needed for the website or not.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,15 @@ import About from "@/components/About";
 import Projects from "@/components/Projects";
 import Timeline from "@/components/Timeline";
 import Footer from "@/components/Footer";
+import React from 'react';
+import ReactGA from 'react-ga4'; // ga4 import
 
 export default function Home() {
+
+  const TRACKING_ID = "G-4TRT9PDZQ9"; // Change tracking ID to use your own on Google Analytics. ID currently set is used for my development environment.
+  ReactGA.initialize(TRACKING_ID);
+  // ReactGA.pageview(document.location.pathname); // This line is currently causing 'ReferenceError: document is not defined in reactjs'
+
   return (
     <>
       <main>
